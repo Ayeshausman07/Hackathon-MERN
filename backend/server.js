5dcc4a45434fc40ac3017dfc001b33d1551c38c2
Ayeshausma07 <uayesha832@gmail.com>
2025-08-08 12:34:51 +0500

    serverupdated

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://hackathon-mern-frontend.vercel.app', credentials: true }));
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
